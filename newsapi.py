10ab8f6ae560f74018e099548ca3d7d686458de5
niranjan310504 <chirdeniranjan31@gmail.com>
2023-11-12 07:59:20 +0530

    Adding my first project

---
+++ newsapi.py
@@ -0,0 +1,14 @@
+import requests
+
+# Set your API key from newsapi.org
+api_key = '0fad3152fa5746698931d1bb504f562b'
+
+# Make a request to the News API
+response = requests.get(f'https://newsapi.org/v2/top-headlines?country=in&apiKey=0fad3152fa5746698931d1bb504f562b')
+
+# Convert the response to JSON format
+articles = response.json()['articles']
+
+# Print the titles of the top 10 articles
+for article in articles[:10]:
+  print(article['title'])
